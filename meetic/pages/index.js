8022508261e24d5145378773e7df6c112b0c2997
Gary <usualpro@gmail.com>
2021-02-08 13:33:33 +0100

    1st commit

---
+++ meetic/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Meetic test-entretien</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <i className="bi-alarm"></i><i className="bi-alarm"></i>
+      </main>
+    </div>
+  )
+}
