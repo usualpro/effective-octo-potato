5631caee5110ccc1470fffb20ff919e2292ca809
Gary <usualpro@gmail.com>
2021-02-15 10:17:46 +0100

    update btn

--- components/Modal.js
+++ components/Modal.js
@@ -2,9 +2,11 @@ import Profile from '../stores/Profile';
 import { observer } from 'mobx-react';
 
 export const Modal = observer(
-    () => <h2>
-        MODAL {JSON.stringify(Profile.modalState.isOpen)}
-        <button onClick={Profile.showModal}>Passe a true</button>
-        <button onClick={Profile.closeModal}>Passe a false</button>
-    </h2>
+    () => (Profile.modalState.isOpen)
+        ? <h2>
+            MODAL {JSON.stringify(Profile.modalState.isOpen)}
+            <button onClick={Profile.showModal}>Passe a true</button>
+            <button onClick={Profile.closeModal}>Passe a false</button>
+        </h2>
+        : null
 );
--- components/ProfileList.js
+++ components/ProfileList.js
@@ -8,7 +8,7 @@ export const ProfileList = observer(
             {
                 Profile[props.data].map(
                     (e, i) => <div className='col-4 col-md-3 col-lg' key={i}>
-                        <button className='ratio ratio-1x1 border-0' onClick={() => Profile.drop(e)}>
+                        <button className='ratio ratio-1x1 border-0' onClick={Profile.showModal}>
                             <img className='object-fit-cover img-fluid rounded' src={e.picture} alt={e.nickname} />
                         </button>
                     </div>
